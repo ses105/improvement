c8f47a6d5e55ec16f0bbc79c58b55bd6f6e13834
ses105 <ciis.yrs.27@gmail.com>
2024-10-05 17:43:37 +0900

    avtivehashの導入

--- db/migrate/20241005080258_create_posts.rb
+++ db/migrate/20241005080258_create_posts.rb
@@ -1,7 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-
+      t.string     :title,        null: false
+      t.text       :question,     null: false
+      t.integer    :category_id,  null: false
+      t.integer    :scheduled_id, null: false
+      t.references :user,         null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_05_071642) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_05_080258) do
+  create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "question", null: false
+    t.integer "category_id", null: false
+    t.integer "scheduled_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -24,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_05_071642) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
